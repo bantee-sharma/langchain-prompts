48a81fd8ed52c62112ef987db4728ffb3462f84e
bantee-sharma <banteesharma9@gmail.com>
2025-05-21 21:45:03 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,19 +5,20 @@ from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 # Load environment variables from .env file (e.g., API keys)
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
+# Initialize the Google Gemini model for chat
+llm = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
 
-chat_history = [
-    SystemMessage(content="You are a helpfull AI")
-]
+
+chat_history = [SystemMessage(content="You are a helpful and intelligent AI assistant.")]
 
 while True:
-    input_text = input("You: ")
-    chat_history.append(HumanMessage(content=input_text))
-    if input_text == "exit":
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input.strip().lower() in ["exit","quit"]:
+        print("AI: Goodbye! Have a great day!")
         break
-    res = model.invoke(chat_history)
-    chat_history.append(AIMessage(content=res.content))
-    print("AI: ",res.content)
+    else:
+        result = llm.invoke(chat_history)
+        chat_history.append(AIMessage(content=result.content))
+        print("AI: ",result.content)
 print(chat_history)
-
